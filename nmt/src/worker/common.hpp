7159eb1236db1b599c45033020a0035529be34fc
Vinicius Barboza <45924164+barbozav@users.noreply.github.com>
2019-02-05 00:57:31 -0200

    feat(nmt): removing C++ RAbbitMQ consumer

--- nmt/src/worker/common.hpp
+++
@@ -1,6 +0,0 @@
-#define CONSUMER_DEFAULT_PERIOD   (1)
-#define RABBITMQ_DEFAULT_HOST     ("localhost")
-#define RABBITMQ_DEFAULT_PORT     (5672)
-#define RABBITMQ_DEFAULT_QUEUE    ("machine-translations")
-#define RABBITMQ_DEFAULT_USERNAME ("guest")
-#define RABBITMQ_DEFAULT_PASSWORD ("guest")
